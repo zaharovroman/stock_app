565257cfac890e5bc4e985962e294e5d705bedd2
Roman Zakharov <rom.zaharov17@gmail.com>
2022-03-16 22:19:21 +0300

    ui fixes

--- lib/presentation/widgets/stock_card.dart
+++ lib/presentation/widgets/stock_card.dart
@@ -12,47 +12,60 @@ class StockCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 6),
-                child: Text(
-                  stock.name,
-                  style: const TextStyle(fontWeight: FontWeight.w600),
-                ),
-              ),
-              Text(
-                stock.symbol,
-                style: const TextStyle(color: Colors.grey),
-              ),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 6),
-                child: Text(
-                  '${stock.value.toString()} ${stock.currency}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          16,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      stock.name,
+                      style: const TextStyle(fontWeight: FontWeight.w600),
+                      softWrap: true,
+                    ),
                   ),
-                ),
+                  Text(
+                    stock.symbol,
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ],
               ),
-              Text(
-                '${stock.change} ${stock.currency} (${stock.changePercent}%)',
-                style: TextStyle(
-                  color: stock.changePercent > 0 ? Colors.green : Colors.red,
-                ),
-              )
-            ],
-          )
-        ],
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      '${stock.value.toString()} ${stock.currency}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '${stock.change} ${stock.currency} (${stock.changePercent}%)',
+                    style: TextStyle(
+                      color: stock.changePercent > 0 ? Colors.green : Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
